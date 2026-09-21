a06f8324e83046023b89804e0411f65c1694e377
Dylan Glandian <dylanglandian@gmail.com>
2026-09-21 19:25:57 +0000

    update

--- Java/000_Labs/010_random_method-static/base_code/starter.java
+++ Java/000_Labs/010_random_method-static/base_code/starter.java
@@ -1,6 +1,6 @@
 /*
- *	Author:  
- *  Date: 
+ *	Author:  Dylan Glandian
+ *  Date: 9/18/26
 */
 
 import java.util.Scanner;
@@ -8,8 +8,22 @@ import java.util.Random;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		System.out.println("Enter 2 numbers to create a range for your random number.");
+		System.out.print("Please enter an integer");
+
+		Scanner sc = new Scanner(system.in);
+		int small = sc.nextInt();
+
+		System.out.print("Please enetr a 2nd integer (larger than first):");
+		int large + sc.nextInt();
+
+		System.out.println("Your range is " + small + "to" + large);
+		int num1 + (int)(Math.random()*(large-small)+small);
+		int num2 + (int)(Math.random()*(large-small)+small);
+		int num3 + (int)(Math.random()*(large-small)+small);
+		int num4 + (int)(Math.random()*(large-small)+small);
+		int num5 + (int)(Math.random()*(large-small)+small);
+		System.out.println(num1 + "," + num2 + "," + num3 + "," + num4 + "," + num5);
+		 )
 	}
 }
